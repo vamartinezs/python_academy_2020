87fd8bd9a65dbd99358db2b248185b6c98bb66ac
vams <va.martinez@globant.com>
2020-09-14 17:30:37 -0500

    [Including the regex Method]

--- week2/zen_of_python_exercise/main.py
+++ week2/zen_of_python_exercise/main.py
@@ -2,8 +2,18 @@ import this
 import codecs
 
 new_block = "".join(map(lambda x: "*" if x.lower() in "aeiou" else x, codecs.encode(this.s, 'rot13')))
-counter = new_block.count('*')
 
+print("## Solving it with Lambda function ##")
+counter = new_block.count('*')
+print("{} \n\nCounter of Vowels: {}".format(new_block, new_block.count('*')))
 print("===============================================================================================================")
-print(new_block)
-print("vowels counter ", counter)
+
+
+
+
+print("##  Try to solve it with Regex ##")
+
+import re
+new_re_block = re.sub("a|e|i|o|u|A|E|I|O|U", "*", codecs.encode(this.s, 'rot13'))
+print("===============================================================================================================")
+print("{} \n\nCounter of Vowels: {}".format(new_re_block,new_re_block.count('*')))
